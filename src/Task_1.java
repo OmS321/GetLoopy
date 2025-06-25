5d050375fb09fa6307e141c5be87f94e731afb03
omsat <omsivasundar2010@gmail.com>
2025-06-25 16:20:21 -0400

    Added Task 1

---
+++ src/Task_1.java
@@ -0,0 +1,14 @@
+public class Task_1
+{
+    public static void main(String[] args)
+    {
+
+        int x = 0;
+
+        for (int i = 0; i <= 30; i++)
+        {
+            System.out.print(x + " ");
+            x++;
+        }
+    }
+}
